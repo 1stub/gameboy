e2a8f7ce8cbedd363ae48a2fa5937cd91e446171
1stub <stubbiegaming@gmail.com>
2024-11-16 16:47:32 -0500

    added memory register header

--- cpu.h
+++ cpu.h
@@ -1,6 +1,50 @@
 #ifndef CPU_H
 #define CPU_H
 
-#include <stdio.h>
+#include "util.h"
+
+struct cpu{
+    bool ime;
+};
+
+typedef struct {
+    union {
+        struct {
+            byte f;
+            byte a;
+        };
+        word af;
+    };
+} reg_af;
+
+typedef struct {
+    union {
+        struct {
+            byte c;
+            byte b;
+        };
+        word bc;
+    };
+} reg_bc;
+
+typedef struct {
+    union {
+        struct {
+            byte e;
+            byte d;
+        };
+        word de;
+    };
+} reg_de;
+
+typedef struct {
+    union {
+        struct {
+            byte l;
+            byte h;
+        };
+        word hl;
+    };
+} reg_hl;
 
 #endif
---
+++ util.h
@@ -0,0 +1,9 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <stdio.h>
+
+typedef unsigned short word;
+typedef unsigned char byte;
+
+#endif
